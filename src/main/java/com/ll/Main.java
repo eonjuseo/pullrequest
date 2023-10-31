695742d1fbac2954df466915ff6dfe4a087593be
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-10-31 09:15:30 +0900

    Third commit

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("새 깃허브 레포를 만들고 pr 하나를 생성하세요");
-        System.out.println("pr에는 3개 이상의 커밋이 있어야 합니다");
+        System.out.println("pullrequest에는 3개 이상의 커밋이 있어야 합니다.");
 
     }
 }
